c631c512042b51f2673e9a67c3c0ae9c19c7e46f
Alan Marchesan <justcavalli@gmail.com>
2012-09-27 13:41:29 +0200

    Add ExFat and NTFS support

--- Android.mk
+++ Android.mk
@@ -12,6 +12,8 @@ common_src_files := \
 	Process.cpp \
 	Ext4.cpp \
 	Fat.cpp \
+	Ntfs.cpp \
+	Exfat.cpp \
 	Loop.cpp \
 	Devmapper.cpp \
 	ResponseCode.cpp \
--- Volume.cpp
+++ Volume.cpp
@@ -44,6 +44,8 @@
 #include "VolumeManager.h"
 #include "ResponseCode.h"
 #include "Fat.h"
+#include "Ntfs.h"
+#include "Exfat.h"
 #include "Process.h"
 #include "cryptfs.h"
 
@@ -397,7 +399,7 @@ int Volume::mountVol() {
 
         errno = 0;
         setState(Volume::State_Checking);
-
+#if 0
         if (Fat::check(devicePath)) {
             if (errno == ENODATA) {
                 SLOGW("%s does not contain a FAT filesystem\n", devicePath);
@@ -409,7 +411,7 @@ int Volume::mountVol() {
             setState(Volume::State_Idle);
             return -1;
         }
-
+#endif
         /*
          * Mount the device on our internal staging mountpoint so we can
          * muck with it before exposing it to non priviledged users.
@@ -425,11 +427,37 @@ int Volume::mountVol() {
             // For secondary external storage we keep things locked up.
             gid = AID_MEDIA_RW;
         }
-        if (Fat::doMount(devicePath, "/mnt/secure/staging", false, false, false,
-                AID_SYSTEM, gid, 0702, true)) {
-            SLOGE("%s failed to mount via VFAT (%s)\n", devicePath, strerror(errno));
-            continue;
-        }
+
+        if( !Exfat::check(devicePath) )
+		{
+			if( Exfat::doMount(devicePath, "/mnt/secure/staging", false, false, false, AID_SYSTEM, gid, 0702, true) )
+			{
+				SLOGE("%s failed to mount via ExFat (%s)\n", devicePath, strerror(errno));
+				continue;
+			}
+		}
+	else if( !Fat::check(devicePath))
+		{
+			if( Fat::doMount(devicePath, "/mnt/secure/staging", false, false, false, AID_SYSTEM, gid, 0702, true) )
+			{
+				SLOGE("%s failed to mount via VFAT (%s)\n", devicePath, strerror(errno));
+				continue;
+			}
+		}
+	else if( !Ntfs::check(devicePath))
+		{
+			if( Ntfs::doMount(devicePath, "/mnt/secure/staging", false, false, false, AID_SYSTEM, gid, 0702, true) )
+			{
+				SLOGE("%s failed to mount via NTFS (%s)\n", devicePath, strerror(errno));
+				continue;
+			}
+		}
+	else
+		{
+			SLOGE("%s failed FS checks (%s)", devicePath, strerror(errno));
+			setState(Volume::State_Idle);
+			continue;
+	}
 
         SLOGI("Device %s, target %s mounted @ /mnt/secure/staging", devicePath, getMountpoint());
 
